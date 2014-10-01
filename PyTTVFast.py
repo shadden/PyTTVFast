60a31bf082f7b61b3175500c9be6f8ab1e421bd8
Sam Hadden <shadden@u.northwestern.edu>
2014-10-01 14:22:57 -0500

    I'm confused

--- PyTTVFast.py
+++ PyTTVFast.py
@@ -110,7 +110,7 @@ class libwrapper(object):
 				return True
 			except RuntimeError:
 				print "Failed"
-				print "Parameters: ", " ".join( map(lambda x: "%.3g"%x, pars[2:]))
+				print "Parameters: ", " ".join( map(lambda x: "%.3f"%x, pars[2:]))
 				return False
 #
 class TTVCompute(object):
@@ -144,14 +144,9 @@ class TTVCompute(object):
 			# this should be fixed!
 			eccs = np.zeros(nplanets)
 			
-<<<<<<< HEAD
 		# Don't let the periapse set timestep less than 1/10th the planet period
 		dtfactors =  np.maximum( np.power( (1. - eccs ) ,1.5) , 0.1 )
 		dt = dtfrac * np.min( periods * dtfactors )
-=======
-
-		dt = dtfrac * np.min(periods * np.power( (1. - eccs ) ,1.5) )
->>>>>>> 24160e279c6048422e62c992503d48873ede50ac
 		
 		n_events = int(np.sum(np.ceil( (tfin-t0) / periods + 1) )) + 1
 		
