c3109685c206d30b3a8332d009bcfc23448d3a00
Samuel Hadden <shadden1107@gmail.com>
2014-11-07 14:03:38 -0600

    make and save artificial data to subdirectory

--- best2mock.py
+++ best2mock.py
@@ -7,21 +7,45 @@ def linefit2(x,y,sigma=None):
 	linefn = lambda x,slope,intercept: x*slope + intercept
 	return curve_fit(linefn,x,y,sigma=sigma)
 
+def linefit_resids(x,y,sigma=None):
+	s,m = linefit2(x,y,sigma)[0]
+	return y - s*x -m
+
 if __name__=="__main__":
-	# a comment
+	
+	# Open and read transit time files
+	try:
+		with open('planets.txt') as fi:
+			planetNames = [l.strip() for l in fi.readlines()]
+		nbody_fit=TTVFitnessAdvanced([np.loadtxt(f) for f in planetNames])
 
-	def linefit_resids(x,y,sigma=None):
-		s,m = linefit2(x,y,sigma)[0]
-		return y - s*x -m
-	#
+	except:
+		raise Exception("Planets file(s) not found!")
 
-	with open('planets.txt') as fi:
-		planetNames = [l.strip() for l in fi.readlines()]
+	# Load best-fit parameters
+	
+	try:
+		transitTimes,sucess = nbody_fit.CoplanarParametersTransformedTransits(np.loadtxt('bestpars.txt'))
+	except:
+		raise Exception("No file `bestpars.txt' found.")
 
-	nbody_fit = nTTV.TTVFitnessAdvanced([np.loadtxt(f) for f in planetNames])
+		
 	
-	bestpars = np.loadtxt('bestpars.txt')
-	best_ttimes,success = nbody_fit.CoplanarParametersTransformedTransits(bestpars)
-	
-	
-	
+	# Make a subdirectory for artificial transit times, make a plot of the artificial TTVs
+	# 	and save the artificial data.
+
+	os.system('mkdir -p ./Artificial')
+	fi = open("Artificial/planets.txt","w")
+	for i,times in enumerate(transitTimes):
+		noiseLvl = median(nbody_fit.transit_uncertainties[i])
+		nTransits = len(times)
+		noise = np.random.normal(0.0,noiseLvl,nTransits	)
+		noisyTimes = times + noise
+		np.savetxt("Artificial/planet%d.txt"%i,vstack(( arange(nTransits) , noisyTimes, noiseLvl * np.ones(nTransits) ) ).T)
+		fi.write("planet%d.txt\n"%i)
+		t0,p  = linefit( np.arange(nTransits) ,noisyTimes)
+		errorbar(noisyTimes, noisyTimes - p* arange(nTransits) - t0 ,yerr=noiseLvl ,fmt='s') 
+
+	savefig('Artificial/Artificial_Transits.png')
+		
+	show()		
